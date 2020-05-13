7c1b36057d5d5abe61a6d4257f379213b7b504ba
tdhhieu59 <65262748+tdhhieu59@users.noreply.github.com>
2020-05-13 09:02:56 +0700

    Add files via upload

---
+++ mymath.py
@@ -0,0 +1,11 @@
+## mymath.py ##
+def  vuông ( n ):
+    trở lại  n * n
+def  khối ( n ):
+    trở lại  n * n
+def  trung bình ( giá trị ):
+    n số = len ( giá trị )
+    tổng = 0,0
+    cho  v  trong  các giá trị :
+        tổng  + =  v
+    return  float ( sum ) / số
