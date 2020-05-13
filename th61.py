e4aab7a24a17e8f46bdb6c94f1f0e026fb2fd80b
tdhhieu59 <65262748+tdhhieu59@users.noreply.github.com>
2020-05-13 09:00:43 +0700

    Add files via upload

---
+++ th61.py
@@ -0,0 +1,8 @@
+lớp  Cricle ( đối tượng ):
+    def  __init__ ( tự , r ):
+        tự . r = r
+     khu vực def ( tự ):
+         tự trở về . r ** 2 * 3.14
+r = int ( đầu vào ( "nhap ban kinh:" ))
+c = Cricle ( r )
+in ( "dien tich hinh tron ​​la:" , c . area ())   
---
+++ th63.py
@@ -0,0 +1,15 @@
+lớp  Nguoi ( đối tượng ):
+    def  getGender ( tự ):
+        trả lại  "Unkn"
+
+lớp  Nam ( Nguoi ):
+    def  getGender ( tự ):
+        trả lại  "Nam"
+lớp  Nu ( Nguoi ):
+    def  getGender ( tự ):
+        trả lại  "Nu"
+    
+a  Nam =  Nam ()
+aNu  =  Nu ()
+in ( a Nam . getGender ())
+in ( aNu . getGender ())
